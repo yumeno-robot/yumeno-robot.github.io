f2c15f31c58b47a3fc217554784a42d90578da51
yumeno-robot <156515161+yumeno-robot@users.noreply.github.com>
2024-08-17 09:47:43 +0900

    gtaed

--- asetts/javascript/script.js
+++ asetts/javascript/script.js
@@ -8,10 +8,8 @@ function showSlides() {
 
     slideIndex++;
     if (slideIndex > slides.length) { slideIndex = 1 }
-
     // スライドの位置を計算し、横にスライドさせる
     slideshowContainer.style.transform = `translateX(-${(slideIndex - 1) * 100}%)`;
-
     // ドットのアクティブ状態を更新
     for (let i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
@@ -19,7 +17,7 @@ function showSlides() {
     dots[slideIndex - 1].className += " active";
 
     // 次のスライドに切り替える
-    setTimeout(showSlides, 10000); // 10秒ごとにスライドを自動的に変更
+    setTimeout(showSlides, 20000); // 10秒ごとにスライドを自動的に変更
 }
 
 function currentSlide(n) {
